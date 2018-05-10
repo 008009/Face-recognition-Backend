c0ae475cea7baaa39cbc019c1e3917ad50fa9c8f
Carl0809 <xrln12345@gmail.com>
2018-05-10 18:45:07 +0800

    changing port

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.post('/imageUrl', (req, res)=> {imageUrl.imageUrlHandler(req, res)})
 // 	console.log(`app is running on ${process.env.PORT}`);
 // });
 
-app.listen(3000, ()=>{
-	console.log(`app is running on 3000`);
+app.listen(3001, ()=>{
+	console.log(`app is running on 3001`);
 });
